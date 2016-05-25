68631699668577a3ccb6c6bd06308acadf793d85
James DiGioia <james.digioia@valtech.com>
2016-05-25 10:21:54 -0400

    Create Downstreams for managing subcomponents
    This functions takes an array of children definitions and transforms
    them into a combined stream + render function. This allows a parent
    component to manage it's children as if it was a single stream.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { curry, identity, pipe } from 'ramda';
+import { pool } from 'kefir';
+
 /**
  * Create a new event with the custom type string.
  *
@@ -19,3 +22,58 @@ export function createEvent(type) {
 
     return event;
 }
+
+/**
+ * Create a combined stream & renderer for an array of child components.
+ *
+ * @param {Object[]} children - Array of child compoent definitions.
+ * @param {Element} el - Parent component element.
+ * @param {Object} state - Current page state.
+ * @returns {stream} Combined child streams.
+ * @factory
+ */
+export const Downstreams = curry(function Downstreams(children, el, state) {
+    const stream = pool();
+    const plug = stream.plug.bind(stream);
+
+    // Map over the children definitions and turn
+    // them into component instances.
+    const renderers = children.map(mapToChildren);
+
+    const api = Object.create(stream);
+
+    api.render = function render(update) {
+        renderers.forEach(renderer => renderer(update));
+
+        state = update;
+
+        return api;
+    };
+
+    return api;
+
+    /**
+     * Plugs child into stream returns child's render function.
+     *
+     * @param {Object} child - Child stream configuration.
+     * @param {Function} child.adapter - Function to modify state before passing to child.
+     * @param {Function} child.factory - Child's factory function.
+     * @param {string} child.selector - Child's querySelector string.
+     * @param {Function} child.preplug - Function to modify stream before being plugged in.
+     * @returns {Function} Child's render function.
+     */
+    function mapToChildren({ adapter = identity, factory, selector, preplug = identity } = {}) {
+        let element = el;
+
+        if (selector) {
+            element = el.querySelector(selector);
+        }
+
+        let instance = factory(element, adapter(state));
+
+        // Plug into Downstream's pool
+        pipe(preplug, plug)(instance);
+
+        return pipe(adapter, instance.render);
+    }
+});
