af07975e2a42e1bad0e6cbd625f9c8361acf97c2
James DiGioia <james.digioia@valtech.com>
2016-06-23 11:31:50 -0400

    Add createFixture to util functions
    This will create an instance and element from the given template
    function, component function, and starting state. For use in tests.

--- util/index.js
+++ util/index.js
@@ -39,3 +39,25 @@ export const mapActionTo = curry(function mapActionTo(source, dest, { type, payl
 
     return { type, payload };
 });
+
+/**
+ * Creates object of fixtures for testing.
+ *
+ * @param {Function} component - Component factory function.
+ * @param {Function} template - Associated handlebars template function.
+ * @param {Object} state - Initial state
+ * @returns {{el, instance: *}} Fixtures.
+ */
+export function createFixture(component, template, state) {
+    const el = (() => {
+        let html = template(state);
+        let result = document.createElement('div');
+        result.innerHTML = html;
+
+        return result.children[0];
+    })();
+
+    const instance = component(el, state);
+
+    return { el, instance };
+}
