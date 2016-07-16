17a063f250de198a35c2a00d7dc5328cade6f8f3
James DiGioia <james.digioia@valtech.com>
2016-07-16 19:49:33 -0400

    Remove createEvent
    Dispatching events can be done directly, by returning a stream from
    `onMount`, so there's no need to enable fake DOM events.

--- util/README.md
+++ util/README.md
@@ -2,6 +2,6 @@
 
 Some useful utility functions for brook.
 
-## `createEvent`
+## `mapActionTo`
 
-This function takes an event type string and return an `Event` object. The `Event` object is just the built-in DOM Event object. The `createEvent` function provides a simple, cross-browser solution to event object creation.
+@todo
--- util/index.js
+++ util/index.js
@@ -1,28 +1,6 @@
 import { curry } from 'ramda';
 
 /**
- * Create a new event with the custom type string.
- *
- * This is used for cross-browser consistency, since the IEs
- * still require us to use the deprecated factory functions.
- *
- * @param {string} type - Event type.
- * @returns {Event} Custom dispatchable event.
- */
-export function createEvent(type) {
-    var event;
-
-    try {
-        event = new Event(type, { bubbles: true });
-    } catch (e) {
-        event = document.createEvent('Event');
-        event.initEvent(type, true, false);
-    }
-
-    return event;
-}
-
-/**
  * Maps actions objects of the source type to the destination type.
  *
  * @param {string} source - Action type to change.
