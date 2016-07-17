22063e5456c70a6f3e4132dec02e4b751b0d45a5
James DiGioia <james.digioia@valtech.com>
2016-07-16 23:42:13 -0400

    Add documentation to create-enhancer
    Also fixes a minor bug, where the `systems$` wasn't emitting its
    events back into the `actions$` stream, instead dispatching directly
    to the legacy `_dispatch`.

--- create-enhancer.js
+++ create-enhancer.js
@@ -1,5 +1,22 @@
 import { constant, fromESObservable, merge, pool } from 'kefir';
 
+/**
+ * Accepts a set of system generator functions and returns a
+ * StoreEnhancer, which generates the `systems$` stream and
+ * binds it to the store.
+ *
+ * Functions similar to middleware, but provides a way to respond
+ * to events from, and emit events into, the application's central
+ * redux store.
+ *
+ * The system function takes an `actions$` stream and a `state$`
+ * stream, and should return a new `system$`, which gets observed by
+ * the store. Note that the `actions$` stream will re-emit
+ * events emitted from the returned `system$`.
+ *
+ * @param {...Function} systems - System-generating function.
+ * @returns {StoreEnhancer} Enhanced create store function.
+ */
 export default function enhancer(...systems) {
     return createStore => (reducer, preloadedState, enhancer) => {
         const store = createStore(reducer, preloadedState, enhancer);
@@ -17,6 +34,6 @@ export default function enhancer(...systems) {
 
         actions$.plug(systems$);
 
-        return Object.assign({}, store, { dispatch: value }, systems$.observe({ value: _dispatch }));
+        return Object.assign({}, store, { dispatch: value }, systems$.observe({ value }));
     };
 }
