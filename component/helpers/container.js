4ff420fe88d38f436e88fb11860741c3aad878ae
James DiGioia <james.digioia@valtech.com>
2016-07-17 12:40:05 -0400

    Add documentation to helpers

--- component/helpers/container.js
+++ component/helpers/container.js
@@ -1,5 +1,11 @@
 import { CONTAINER_ATTRIBUTE } from '../events';
 
+/**
+ * Generates a HTML attribute for a container.
+ *
+ * @param {string} name - Container name.
+ * @returns {string} Container attribute.
+ */
 export default function container(name) {
     return `${CONTAINER_ATTRIBUTE}="${name}"`;
 }
--- component/helpers/event.js
+++ component/helpers/event.js
@@ -1,5 +1,12 @@
 import { EVENT_ATTRIBUTES } from '../events';
 
+/**
+ * Generates HTML event attribute for an element.
+ *
+ * @param {string} event - Event name.
+ * @param {string} callback - Callback key.
+ * @returns {string} Event attribute.
+ */
 export default function event(event, callback) {
     const attr = EVENT_ATTRIBUTES[event];
 
