dd98e4f17b5339cc8a07117c34bc3dad8c4959e1
mAAdhaTTah <jamesorodig@gmail.com>
2017-01-29 17:25:52 -0500

    Add support for events in silt
    Some of the internals got moved around to indicate the difference
    between `onMount` and the other types. Also, add requirements for
    `container` attribute, to make the sure that is properly implemented.

--- src/silt/__tests__/silt.spec.js
+++ src/silt/__tests__/silt.spec.js
@@ -2,34 +2,107 @@
 import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import simulant from 'simulant';
 
+import R from 'ramda';
 import { never, pool } from 'kefir';
 
+import { CLICK, CONTAINER_ATTRIBUTE, EVENT_ATTRIBUTES, KEYPRESS } from '../../constants';
+import { containerAttribute } from '../../helpers';
 import silt from '../';
 
 chai.use(sinonChai);
 
 describe('silt', () => {
-    describe('template tag', () => {
-        it('should return a function', () => {
-            const Component = silt`<div></div>`;
+    it('should throw if no container attribute provided', () => {
+        expect(() => silt`<div></div>`).to.throw(Error);
+    });
 
-            expect(Component).to.be.a('function');
+    it('should return a function', () => {
+        const Component = silt`<div ${containerAttribute('fixture')}></div>`;
+
+        expect(Component).to.be.a('function');
+    });
+
+    it('should call onMount', () => {
+        const spy = sinon.spy(() => never());
+        const onMount = silt.onMount(spy);
+        const Component = silt`<div ${containerAttribute('fixture')} ${onMount}></div>`;
+        const fixture = document.createElement('div');
+        const props$ = never();
+
+        const sub = Component(fixture, props$).observe({});
+
+        expect(spy).to.have.callCount(1);
+        expect(spy).to.have.been.calledWith(fixture, props$);
+
+        sub.unsubscribe();
+    });
+
+    it('should emit event', () => {
+        const event = { type: 'CLICK' };
+        const spy = sinon.spy(e$ => e$.map(R.always(event)));
+        const click = silt.event(CLICK, spy);
+        const Component = silt`<div ${containerAttribute('fixture')} ${click}></div>`;
+        const fixture = document.createElement('div');
+        fixture.setAttribute(CONTAINER_ATTRIBUTE, 'fixture');
+        fixture.setAttribute(EVENT_ATTRIBUTES[CLICK], click.$$key);
+        const props$ = pool();
+        let called = false;
+        document.body.appendChild(fixture);
+        const sub = Component(fixture, props$.toProperty(R.always({}))).observe({
+            value(e) {
+                called = true;
+                expect(e).to.equal(event);
+            }
         });
+        simulant.fire(fixture, 'click');
 
-        it('should call onMount', () => {
-            const spy = sinon.spy(() => never());
-            const onMount = silt.onMount(spy);
-            const Component = silt`<div ${onMount}></div>`;
-            const fixture = document.createElement('div');
-            const props$ = pool();
+        expect(spy).to.have.callCount(1);
+        expect(called).to.equal(true);
 
-            const sub = Component(fixture, props$).observe({});
+        document.body.removeChild(fixture);
+        sub.unsubscribe();
+    });
 
-            expect(spy).to.have.callCount(1);
-            expect(spy).to.have.been.calledWith(fixture, props$);
+    it('should handle multiple events', () => {
+        const clickAction = { type: 'CLICK' };
+        const clickCallback = sinon.spy(e$ => e$.map(R.always(clickAction)));
+        const click = silt.event(CLICK, clickCallback);
 
-            sub.unsubscribe();
+        const keypressAction = { type: 'KEYPRESS' };
+        const keypressCallback = sinon.spy(e$ => e$.map(R.always(keypressAction)));
+        const keypress = silt.event(KEYPRESS, keypressCallback);
+
+        const Component = silt`<div ${containerAttribute('fixture')} ${click} ${keypress}></div>`;
+
+        const fixture = document.createElement('div');
+        fixture.setAttribute(CONTAINER_ATTRIBUTE, 'fixture');
+        fixture.setAttribute(EVENT_ATTRIBUTES[CLICK], click.$$key);
+        fixture.setAttribute(EVENT_ATTRIBUTES[KEYPRESS], keypress.$$key);
+
+        const props$ = pool();
+
+        let called = 0;
+        document.body.appendChild(fixture);
+        const sub = Component(fixture, props$.toProperty(R.always({}))).observe({
+            value(e) {
+                if (!called) {
+                    expect(e).to.equal(clickAction);
+                } else {
+                    expect(e).to.equal(keypressAction);
+                }
+
+                called++;
+            }
         });
+        simulant.fire(fixture, 'click');
+        simulant.fire(fixture, 'keypress');
+
+        expect(clickCallback).to.have.callCount(1);
+        expect(called).to.equal(2);
+
+        document.body.removeChild(fixture);
+        sub.unsubscribe();
     });
 });
--- src/silt/index.js
+++ src/silt/index.js
@@ -1,23 +1,27 @@
 import R from 'ramda';
 import Handlebars from 'handlebars';
+import { never } from 'kefir';
+import { eventAttribute } from '../helpers';
 import component from '../component';
 import children from '../children';
 import events from '../events';
 import render from '../render';
 
-const $$helper = '$$helper';
+const $$attribute = '$$attribute';
 const $$config = '$$config';
+const $$key = '$$key';
+const $$onMount = '$$onMount';
 
 /**
  * Tagged template function for template-based component declarations.
  *
  * @param {string[]} strings - Template strings.
- * @param {function[]} values - Component lifecycle functions.
+ * @param {Array<string|Function>} values - Component lifecycle functions.
  * @returns {factory} Component factory function.
  */
 const silt = function silt(strings, ...values) {
     const opts = {
-        onMount: R.identity,
+        onMount: R.always(never()),
         children: {},
         events: {}
     };
@@ -26,12 +30,32 @@ const silt = function silt(strings, ...values) {
         values.push(R.identity);
     }
 
+    let hasContainer = false;
+
     const template = R.zip(strings, values).reduce(
         (acc, [string, func]) => {
-            opts[func[$$config]] = func;
+            if (typeof func === 'string') {
+                if (func.indexOf('data-brk-container') !== -1) {
+                    hasContainer = true;
+                }
 
-            return acc + string + func[$$helper];
+                return acc + string + func;
+            }
+
+            if (func[$$onMount]) {
+                opts.onMount = func;
+            } else if (func[$$config]) {
+                Object.assign(opts[func[$$config]], {
+                    [func[$$key]]: func
+                });
+            }
+
+            return acc + string + (func[$$attribute] || '');
         }, '');
+
+    if (!hasContainer) {
+        throw new Error('Component does not have container');
+    }
 
     return component({
         onMount: opts.onMount,
@@ -49,11 +73,33 @@ const silt = function silt(strings, ...values) {
  */
 export const onMount = silt.onMount = function onMount(func) {
     return Object.defineProperties(func, {
+        [$$onMount]: {
+            value: true
+        }
+    });
+};
+
+let id = 0;
+
+/**
+ * Tags a function as an event function for use by silt.
+ *
+ * @param {string} type - Event type.
+ * @param {Function} func - Function to tag
+ * @returns {Function} Function with tagged attributes.
+ */
+export const event = silt.event = function event(type, func) {
+    const key = `silt${id++}`;
+
+    return Object.defineProperties(func, {
         [$$config]: {
-            value: 'onMount'
+            value: 'events'
         },
-        [$$helper]: {
-            value: ''
+        [$$attribute]: {
+            value: eventAttribute(type, key)
+        },
+        [$$key]: {
+            value: key
         }
     });
 };
