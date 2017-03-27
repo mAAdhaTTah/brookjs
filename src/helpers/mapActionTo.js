965481dd6e433031c218ecd3475350117800e827
mAAdhaTTah <jamesorodig@gmail.com>
2017-03-26 22:02:47 -0400

    Update `mapActionTo` and add tests
    `mapActionTo` uses the `meta` to track the sources, to conform
    with the Flux Standard Action. This deprecates the source on the
    body of the object. Comes with a warning message, which we
    can remove in future release.

--- src/helpers/mapActionTo.js
+++ src/helpers/mapActionTo.js
@@ -1,10 +1,23 @@
 import R from 'ramda';
 
-export default R.curry(function mapActionTo(source, dest, { type, payload }) {
-    if (type === source) {
-        type = dest;
-        payload = Object.assign({}, payload, { source });
+export default R.curry(function mapActionTo(source, dest, action) {
+    if (action.type !== source) {
+        return action;
     }
 
-    return { type, payload };
+    const { meta = {}, ...rest } = action;
+
+    return {
+        ...rest,
+        type: dest,
+        meta: {
+            ...meta,
+            sources: (meta.sources || []).concat(source)
+        },
+        get source() {
+            console.warn('`source` is now located at `meta.sources`');
+
+            return source;
+        }
+    };
 });
