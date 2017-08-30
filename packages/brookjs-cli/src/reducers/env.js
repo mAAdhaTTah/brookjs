a1846e7fd973bcc89015fc3b4a760ce5515a366a
mAAdhaTTah <jamesorodig@gmail.com>
2017-08-29 23:07:01 -0400

    Set RC file data into the store
    We need to read the dir from the rc file. The .beaverrc.js
    currently generated by brookjs doesn't _actually_ work yet :/
    because we need to wait for `@std/esm` to allow us to compile
    modules outside of the package. A commonjs module works
    fine though.

--- packages/brookjs-cli/src/reducers/env.js
+++ packages/brookjs-cli/src/reducers/env.js
@@ -1,15 +1,19 @@
 import R from 'ramda';
 import { combineActionReducers } from 'brookjs';
-import { READ_ENV } from '../actions';
-import { lCwd } from '../lenses';
+import { READ_ENV, READ_RC_FILE } from '../actions';
+import { lCwd, lDir } from '../lenses';
 
 const defaults = {
-    cwd: ''
+    cwd: '',
+    dir: ''
 };
 
 const cond = [
     [READ_ENV, (state, { payload }) => R.pipe(
         R.set(lCwd, payload.cwd)
+    )(state)],
+    [READ_RC_FILE, (state, { payload }) => R.pipe(
+        R.set(lDir, payload.dir)
     )(state)]
 ];
 
