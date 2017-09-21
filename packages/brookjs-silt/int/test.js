f62d33dc7d4baeb7ee351be2110f54c185ff375d
mAAdhaTTah <jamesorodig@gmail.com>
2017-09-20 21:46:05 -0400

    Set up initial tape tests for integration
    This allows us create some integration tests to make sure the
    vdom generated by parsing the generated handlebars template
    matches the vdom generated by the same template generated
    by silt.

---
+++ packages/brookjs-silt/int/test.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import fs from 'fs';
+import glob from 'glob';
+import Kefir from 'kefir';
+import hbs from 'handlebars';
+import test from 'tape';
+import { Internals } from 'diffhtml/dist/cjs';
+import { parse, generate } from '../es';
+
+test('integration', t => {
+    Kefir.fromNodeCallback(callback => glob(path.join(__dirname, '*.int.hbs'), callback))
+        .flatten()
+        .observe({
+            value: filename => {
+                const [, name] = filename.match(/(\w+)\.int\.hbs/);
+
+                t.test(name, t => {
+                    t.plan(1);
+
+                    const contents = fs.readFileSync(filename).toString();
+                    const handlebars = context => {
+                        const func = hbs.compile(contents);
+                        const text = func(context);
+                        return Internals.parse(text);
+                    };
+                    const silt = generate(parse(contents));
+                    const context = require(`./${name}.json`);
+
+                    const actual = silt(context);
+                    const expected = handlebars(context);
+
+                    t.deepEqual(actual, expected);
+                });
+            },
+            end: () => t.end()
+        });
+});
