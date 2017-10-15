3628a1e35d451c1e7f3cb3fc751c5e2cf852b291
mAAdhaTTah <jamesorodig@gmail.com>
2017-10-14 22:21:28 -0400

    Move program into main bin file
    This is the CLI-only API, so this should go in the bin entry, and leave
    the `main` function in the app.

--- packages/brookjs-cli/bin/beaver.js
+++ packages/brookjs-cli/bin/beaver.js
@@ -1,4 +1,35 @@
 #!/usr/bin/env node
-const { program } = require('../src');
+const program = require('caporal');
+const pkg = require('../package.json');
+const { main } = require('../src');
+
+program
+    .version(pkg.version)
+    .description(pkg.description);
+
+program
+    .command('new', 'Create a new brookjs application.')
+    .action(main('new'));
+
+program
+    .command('make', 'Create a new file of a given type for brookjs application.')
+    .action(main('make'))
+    .argument('<type>', 'Type to make.', ['action', 'component', 'delta', 'reducer', 'selector', 'service'])
+    .argument('<name>', 'Name of the file/type created.');
+
+program
+    .command('dev', 'Develop the brookjs application.')
+    .action(main('test'))
+    .argument('<type>', 'Development style.', ['app', 'tdd']);
+
+program
+    .command('test', 'Run the brookjs application tests.')
+    .action(main('test'))
+    .argument('<type>', 'Tests to run.', ['unit', 'e2e']);
+
+program
+    .command('build', 'Build the brookjs application files.')
+    .action(main('build'))
+    .argument('<env>', 'Build environment target.', ['development', 'production']);
 
 program.parse(process.argv);
--- packages/brookjs-cli/src/index.js
+++ packages/brookjs-cli/src/index.js
@@ -1,4 +1,3 @@
 require = require('@std/esm')(module);
 
-exports.program = require('./program').default;
 exports.main = require('./main').default;
