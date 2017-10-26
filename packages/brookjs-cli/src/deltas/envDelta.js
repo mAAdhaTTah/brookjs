5db10635a0db88b0c616dda0e20eada9535757f8
mAAdhaTTah <jamesorodig@gmail.com>
2017-10-25 22:03:41 -0400

    Fix envDelta to read rc after ENV
    We need the env to read the rc file (for the path), so we need to
    ensure these are sequenced correctly.

--- packages/brookjs-cli/src/deltas/envDelta.js
+++ packages/brookjs-cli/src/deltas/envDelta.js
@@ -1,11 +1,12 @@
 import R from 'ramda';
 import { Kefir } from 'brookjs';
-import { readEnv, readRcFile, readRcFileError, RUN } from '../actions';
+import { readEnv, readRcFile, readRcFileError, RUN, READ_ENV } from '../actions';
 import { selectRcPath } from '../selectors';
 
 export default R.curry(({ process, require }, actions$, state$) => {
     const beaverrc$ = state$.sampledBy(
         actions$.ofType(RUN).take(1).filter(({ payload }) => payload.command !== 'new')
+            .flatMap(() => actions$.ofType(READ_ENV).take(1))
     )
         .flatMap(state => Kefir.stream(emitter => {
             try {
