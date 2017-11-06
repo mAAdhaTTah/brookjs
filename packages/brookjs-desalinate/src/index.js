02d1c3a5a760b71fb751d6f232e8c5ce3b7207d4
mAAdhaTTah <jamesorodig@gmail.com>
2017-11-05 23:03:11 -0500

    Remove createFixture from desalinate
    We're not using this currently, so we can remove it now, which
    eliminates a circular dep between brookjs & desalinate.

--- packages/brookjs-desalinate/src/index.js
+++ packages/brookjs-desalinate/src/index.js
@@ -1,3 +1,2 @@
 export { default as createElementFromTemplate } from './createElementFromTemplate';
-export { default as createFixture } from './createFixture';
 export { cleanup } from './elements';
