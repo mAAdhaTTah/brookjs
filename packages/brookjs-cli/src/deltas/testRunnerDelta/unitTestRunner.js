0bd8347638b122502da82e1c6694f1a74a25a1a2
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-17 16:49:19 -0500

    Make sure unit tests exit with correct code
    After excuting the mocha tests, make sure we exit the main process
    with the same error code. Otherwise, it may accidentally appear as
    if the tests passed when they did not.

--- packages/brookjs-cli/src/deltas/testRunnerDelta/unitTestRunner.js
+++ packages/brookjs-cli/src/deltas/testRunnerDelta/unitTestRunner.js
@@ -6,9 +6,9 @@ import { getMochaCommand } from '../../selectors';
 export default R.curry(({  }, actions$, state$) =>
     state$.take(1).flatMap(state => Kefir.stream(emitter => {
         shelljs.exec(getMochaCommand(state), {
-            async: true,
             shell: '/usr/local/bin/bash'
+        }, code => {
+            process.exitCode = code;
+            emitter.end();
         });
-
-        emitter.end();
     })));
