1932614570136be615675c9b10279f16136b5834
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-17 17:03:03 -0500

    Switch desalinate's tests to brookjs-cli
    Use `beaver test unit` to run the unit tests.

---
+++ packages/brookjs-desalinate/.beaverrc.js
@@ -0,0 +1,27 @@
+export const dir = 'src';
+
+/**
+ * Mocha testing configuration.
+ */
+export const mocha = {
+    reporter: 'spec',
+    ui: 'bdd',
+    requires: [
+        'babel-register',
+        'jsdom-global/register'
+    ]
+};
+
+/**
+ * Webpack build configuration.
+ */
+export const webpack = {
+    entry: {
+        app: 'app.js'
+    },
+    output: {
+        path: 'dist/',
+        filename: '[name].js'
+    },
+    modifier: x => x
+};
