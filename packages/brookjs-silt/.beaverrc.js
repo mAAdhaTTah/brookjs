1c1011a49338031b0edc82c254b147b5706aa3f4
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-17 17:17:58 -0500

    Use beaver test & babel preset in brookjs-silt

---
+++ packages/brookjs-silt/.beaverrc.js
@@ -0,0 +1,26 @@
+export const dir = 'src';
+
+/**
+ * Mocha testing configuration.
+ */
+export const mocha = {
+    reporter: 'spec',
+    ui: 'bdd',
+    requires: [
+        'babel-register'
+    ]
+};
+
+/**
+ * Webpack build configuration.
+ */
+export const webpack = {
+    entry: {
+        app: 'app.js'
+    },
+    output: {
+        path: 'dist/',
+        filename: '[name].js'
+    },
+    modifier: x => x
+};
