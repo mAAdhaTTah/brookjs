881c232740af1fb487934ae54abcda42f05a0cb2
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-17 16:41:32 -0500

    Switch unit tests in brookjs to brookjs-cli
    Use `beaver test unit` for running the tests.

---
+++ packages/brookjs/.beaverrc.js
@@ -0,0 +1,28 @@
+export const dir = 'src';
+
+/**
+ * Mocha testing configuration.
+ */
+export const mocha = {
+    reporter: 'spec',
+    ui: 'bdd',
+    requires: [
+        'babel-register',
+        'raf/polyfill',
+        'jsdom-global/register'
+    ]
+};
+
+/**
+ * Webpack build configuration.
+ */
+export const webpack = {
+    entry: {
+        app: 'app.js'
+    },
+    output: {
+        path: 'dist/',
+        filename: '[name].js'
+    },
+    modifier: x => x
+};
