9e1047fafb933b3b82d319bac613f7e624eaeb1f
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-22 10:08:59 -0500

    Cleanup the terminal UI when command is finished
    After scaffolding & npm install finishes running, we need to end
    the terminal UI or the CLI tool will sit there without exiting.

--- packages/brookjs-cli/src/deltas/terminalDelta/index.js
+++ packages/brookjs-cli/src/deltas/terminalDelta/index.js
@@ -1,5 +1,5 @@
 import { Kefir, ofType } from 'brookjs';
-import { RUN } from '../../actions';
+import { NPM_COMMAND_FINISH, RUN, SCAFFOLD_FINISH } from '../../actions';
 import log from './log';
 import CommandNotFound from './CommandNotFound';
 import NewCommandPrompt from './NewCommandPrompt';
@@ -12,7 +12,13 @@ export default (services) => (actions$, state$) =>
         state$.sampledBy(actions$.thru(ofType(RUN))).take(1).flatMap(state => {
             switch (state.command.name) {
                 case 'new':
-                    return mountTerminal(NewCommandPrompt, state$);
+                    return mountTerminal(NewCommandPrompt, state$)
+                        .takeUntilBy(
+                            // 2 NPM commands are run (dev & prod deps)
+                            actions$.thru(ofType(NPM_COMMAND_FINISH, SCAFFOLD_FINISH))
+                                .bufferWithCount(3)
+                                .take(1)
+                        );
                 case 'dev':
                     return devCommand(services, actions$, state$);
                 case 'make':
