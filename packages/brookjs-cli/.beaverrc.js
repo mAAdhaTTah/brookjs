5c3eebd632cc035ac9bba790930c8b44ea70ccae
mAAdhaTTah <jamesorodig@gmail.com>
2018-01-01 18:40:23 -0500

    Update the .beaverrc.js in `brookjs-cli`

--- packages/brookjs-cli/.beaverrc.js
+++ packages/brookjs-cli/.beaverrc.js
@@ -10,3 +10,33 @@ export const mocha = {
         '@std/esm'
     ]
 };
+
+/**
+ * Webpack build configuration.
+ */
+export const webpack = {
+    entry: {
+        app: 'app.js'
+    },
+    output: {
+        path: 'dist/',
+        filename: '[name].js'
+    },
+    modifier: (config, state) => config
+};
+
+/**
+ * Storybook development environment configuration.
+ *
+ * This will use the webpack configuration defined above.
+ */
+export const storybook = {
+    port: 9001,
+    host: null,
+    staticDirs: [],
+    https: {
+        enabled: false
+    },
+    devServer: {},
+    middleware: (router, state) => router
+};
