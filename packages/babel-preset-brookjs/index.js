d563911e26b4e55b50908d21b13b5a3be693de2b
mAAdhaTTah <jamesorodig@gmail.com>
2018-01-14 16:48:31 -0500

    Add transform jsx to babel preset
    We're going to default to React.createElement, so people can do
    `import * as React from 'brookjs-silt'` and it'll work fine. I'm not
    entirely sure we should _encourage_ this, because then we're
    _kinda_ responsible for making the whole API available, which
    we definitely don't want to do if we want to make the render
    layer swappable.

--- packages/babel-preset-brookjs/index.js
+++ packages/babel-preset-brookjs/index.js
@@ -1,9 +1,10 @@
-module.exports = function(api, { useBuiltIns = true, modules = false, targets } = {}) {
+module.exports = function(api, { pragma = 'React.createElement', useBuiltIns = true, modules = false, targets } = {}) {
     return {
         presets: [
             ['babel-preset-env', { useBuiltIns, modules, targets }]
         ],
         plugins: [
+            ['transform-react-jsx', { pragma }],
             'babel-plugin-ramda',
             'babel-plugin-syntax-dynamic-import',
             ['babel-plugin-transform-object-rest-spread', { useBuiltIns }]
