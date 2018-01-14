31b2d939fd3cb5f0379c3d44c540ff587c2019b4
mAAdhaTTah <jamesorodig@gmail.com>
2018-01-14 16:11:02 -0500

    Add tests for Aggregator
    Ensures the callbacks are called with the correct arguments.

--- packages/brookjs-silt/src/__tests__/events.spec.js
+++ packages/brookjs-silt/src/__tests__/events.spec.js
@@ -5,13 +5,17 @@ import { expect, use } from 'chai';
 import { Kefir } from 'brookjs';
 import { chaiPlugin } from 'brookjs-desalinate';
 import PropTypes from 'prop-types';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import Collector from '../Collector';
 import h from '../h';
+import Aggregator from '../Aggregator';
 
 const { plugin, value } = chaiPlugin({ Kefir });
 
 configure({ adapter: new Adapter() });
 use(plugin);
+use(sinonChai);
 
 describe('events aggregation', () => {
     describe('Collector', () => {
@@ -99,6 +103,46 @@ describe('events aggregation', () => {
             wrapper.unmount();
 
             expect(aggregated$._curSources).to.have.lengthOf(0);
+        });
+    });
+
+    describe('Aggregator', () => {
+        it('should provide aggregated$ as context', () => {
+            const spy = sinon.spy(() => {});
+            mount(
+                <Aggregator action$={spy}>
+                    <p>Hello world!</p>
+                </Aggregator>
+            );
+
+            expect(spy).to.have.callCount(1);
+            expect(spy.getCall(0).args[0]).to.be.observable();
+        });
+
+        it('should call unsubscribe when unmounted', () => {
+            const spy = sinon.spy(x => x.observe());
+            const wrapper = mount(
+                <Aggregator action$={spy}>
+                    <p>Hello world!</p>
+                </Aggregator>
+            );
+
+            wrapper.unmount();
+
+            expect(spy.getCall(0).args[0]).to.not.be.active();
+        });
+
+        it('should function correctly when nothing returned', () => {
+            const spy = sinon.spy(() => {});
+            const wrapper = mount(
+                <Aggregator action$={spy}>
+                    <p>Hello world!</p>
+                </Aggregator>
+            );
+
+            wrapper.unmount();
+
+            expect(spy.getCall(0).args[0]).to.not.be.active();
         });
     });
 });
