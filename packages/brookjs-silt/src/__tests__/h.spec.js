21f6c36130f825d950c96504eaf283dca3e84859
mAAdhaTTah <jamesorodig@gmail.com>
2018-01-14 11:51:05 -0500

    Switch h tests to enzyme
    We're going to check some changes over time with
    these tests, specifically for the implementation
    of `ref`.

--- packages/brookjs-silt/src/__tests__/h.spec.js
+++ packages/brookjs-silt/src/__tests__/h.spec.js
@@ -1,57 +1,64 @@
 /* eslint-env mocha */
-import { expect } from 'chai';
+import { configure, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect, use } from 'chai';
+import { chaiPlugin } from 'brookjs-desalinate';
 import { Kefir } from 'brookjs';
-import ReactDOM from 'react-dom/server';
 import h from '../h';
+
+const { plugin } = chaiPlugin({ Kefir });
+
+configure({ adapter: new Adapter() });
+use(plugin);
 
 describe('h', () => {
     describe('interop', () => {
         it('should return a plain react element', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p>Hello world!</p>);
+            const actual = render(<p>Hello world!</p>);
 
-            expect(actual).to.equal('<p>Hello world!</p>');
+            expect(actual.toString()).to.equal('<p>Hello world!</p>');
         });
 
         it('should return a plain react element with key', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p key="k">Hello world!</p>);
+            const actual = render(<p key="k">Hello world!</p>);
 
-            expect(actual).to.equal('<p>Hello world!</p>');
+            expect(actual.toString()).to.equal('<p>Hello world!</p>');
         });
 
         it('should return plain nested react element', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p>Just testing <span>constants</span>.</p>);
+            const actual = render(<p>Just testing <span>constants</span>.</p>);
 
-            expect(actual).to.equal('<p>Just testing <span>constants</span>.</p>');
+            expect(actual.toString()).to.equal('<p>Just testing <span>constants</span>.</p>');
         });
     });
 
     describe('children', () => {
         it('should render text from observable', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p>{Kefir.constant('Hello world!')}</p>);
+            const actual = render(<p>{Kefir.constant('Hello world!')}</p>);
 
-            expect(actual).to.equal('<p>Hello world!</p>');
+            expect(actual.toString()).to.equal('<p>Hello world!</p>');
         });
 
         it('should render text from observable in array', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p>{[Kefir.constant('Hello world!')]}</p>);
+            const actual = render(<p>{[Kefir.constant('Hello world!')]}</p>);
 
-            expect(actual).to.equal('<p>Hello world!</p>');
+            expect(actual.toString()).to.equal('<p>Hello world!</p>');
         });
 
         it('should render an element from observable', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<div>{Kefir.constant(<p>Hello world!</p>)}</div>);
+            const actual = render(<div>{Kefir.constant(<p>Hello world!</p>)}</div>);
 
-            expect(actual).to.equal('<div><p>Hello world!</p></div>');
+            expect(actual.toString()).to.equal('<div><p>Hello world!</p></div>');
         });
 
         it('should render observable sibling', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p>Hello {Kefir.constant('world!')}</p>);
+            const actual = render(<p>Hello {Kefir.constant('world!')}</p>);
 
-            expect(actual).to.equal('<p>Hello world!</p>');
+            expect(actual.toString()).to.equal('<p>Hello world!</p>');
         });
 
         it('should render deeply nested elements and observables', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
+            const actual = render(
                 <div>
                     <div key="a">a</div>
                     {[<div key="b">b</div>, Kefir.constant([
@@ -61,44 +68,44 @@ describe('h', () => {
                 </div>
             );
 
-            expect(actual).to.equal('<div><div>a</div><div>b</div><div>c</div><div>d</div></div>');
+            expect(actual.toString()).to.equal('<div><div>a</div><div>b</div><div>c</div><div>d</div></div>');
         });
 
         const Spread = props => <div {...props} />;
 
         it('should render children of custom components', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
+            const actual = render(
                 <Spread>
                     Hello {Kefir.constant('world!')}
                 </Spread>
             );
 
-            expect(actual).to.equal('<div>Hello world!</div>');
+            expect(actual.toString()).to.equal('<div>Hello world!</div>');
         });
 
         const ChildrenWithSibling = ({ children }) => <div>Test: {children}</div>;
 
         it('should render children with siblings correctly', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
+            const actual = render(
                 <ChildrenWithSibling>
                     Hello {Kefir.constant('world!')}
                 </ChildrenWithSibling>
             );
 
-            expect(actual).to.equal('<div>Test: Hello world!</div>');
+            expect(actual.toString()).to.equal('<div>Test: Hello world!</div>');
         });
     });
 
     describe('attributes', () => {
         it('should render attribute from observable', () => {
-            const actual = ReactDOM.renderToStaticMarkup(<p id={Kefir.constant('an-id')}>Hello world!</p>);
+            const actual = render(<p id={Kefir.constant('an-id')}>Hello world!</p>);
 
-            expect(actual).to.equal('<p id="an-id">Hello world!</p>');
+            expect(actual.toString()).to.equal('<p id="an-id">Hello world!</p>');
         });
 
         describe('style', () => {
             it('should render style from observable on object property', () => {
-                const actual = ReactDOM.renderToStaticMarkup(
+                const actual = render(
                     <div onClick={() => {}}
                         style={{
                             display: 'block',
@@ -110,7 +117,7 @@ describe('h', () => {
                     </div>
                 );
 
-                expect(actual).to.equal(
+                expect(actual.toString()).to.equal(
                     '<div style="display:block;color:red;background:green">' +
                         '<p>Hello</p>' +
                         '<p>World</p>' +
@@ -119,13 +126,13 @@ describe('h', () => {
             });
 
             it('should render style from observable of object', () => {
-                const actual = ReactDOM.renderToStaticMarkup(
+                const actual = render(
                     <a href="#lol" style={Kefir.constant({ color: 'red' })}>
                         {Kefir.constant('Hello')} {Kefir.constant('world!')}
                     </a>
                 );
 
-                expect(actual).to.equal('<a href="#lol" style="color:red">Hello world!</a>');
+                expect(actual.toString()).to.equal('<a href="#lol" style="color:red">Hello world!</a>');
             });
         });
     });
@@ -136,27 +143,27 @@ describe('h', () => {
         );
 
         it('should behave normally if not embeddable', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
-                <Custom prop={Kefir.constant('not-embeddable')} ref="test"/>
+            const actual = render(
+                <Custom prop={Kefir.constant('not-embeddable')}/>
             );
 
-            expect(actual).to.equal('<div>[constant] {}</div>');
+            expect(actual.toString()).to.equal('<div>[constant] {}</div>');
         });
 
         it('should allow observables to be embedded into custom components', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
-                <Custom silt-embeddable prop={Kefir.constant('embeddable')} ref="test"/>
+            const actual = render(
+                <Custom silt-embeddable prop={Kefir.constant('embeddable')} />
             );
 
-            expect(actual).to.equal('<div>embeddable {}</div>');
+            expect(actual.toString()).to.equal('<div>embeddable {}</div>');
         });
 
         it('should accept regular props if embeddable', () => {
-            const actual = ReactDOM.renderToStaticMarkup(
-                <Custom silt-embeddable prop={'embeddable-anyway'} ref="test"/>
+            const actual = render(
+                <Custom silt-embeddable prop={'embeddable-anyway'}/>
             );
 
-            expect(actual).to.equal('<div>embeddable-anyway {}</div>');
+            expect(actual.toString()).to.equal('<div>embeddable-anyway {}</div>');
         });
     });
 });
