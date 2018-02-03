ed59130c452daaf5e78bea843734818aa1c7d5b2
mAAdhaTTah <jamesorodig@gmail.com>
2018-02-03 16:53:09 -0500

    Add implementation of `loop` as iteration helper
    Takes an `Observable<{ order, dict }>`, which it uses to build up the
    correct Element + stream combination. It also comes with a
    built-in optimization, preventing new elements from being emitted if
    the order doesn't change, and reusing the streams created. This way,
    the reconciler can treat the returned element as the same thing,
    avoid unncessary rerenders.
    
    Fixes #124.

--- packages/brookjs-silt/src/index.js
+++ packages/brookjs-silt/src/index.js
@@ -2,6 +2,7 @@
 import Aggregator from './Aggregator';
 import Collector from './Collector';
 import h from './h';
+import loop from './loop';
 import view from './view';
 
-export { Aggregator, Collector, h, h as createElement, view };
+export { Aggregator, Collector, h, h as createElement, loop, view };
