06c7bc70088197c2b53332bc0fb2aa60938acb70
mAAdhaTTah <jamesorodig@gmail.com>
2018-03-24 19:53:48 -0400

    Remove Webpack Dashboard and just log stats
    This is simple enough to get working, and once we switch to Ink or
    something like it, we'll create our own UI for doing that.
    
    Fix #142.

--- packages/brookjs-cli/src/deltas/terminalDelta/devCommand.js
+++ packages/brookjs-cli/src/deltas/terminalDelta/devCommand.js
@@ -1,28 +1,16 @@
 import R from 'ramda';
 import { ofType, Kefir } from 'brookjs';
-import Dashboard from 'webpack-dashboard';
 import { lCommandTypeArg } from '../../lenses';
-import { WEBPACK_DASHBOARD_EVENTS } from '../../actions';
+import { WEBPACK_COMPILED } from '../../actions';
 
 
-export default (services, actions$, state$) => {
+export default ({ ui }, actions$, state$) => {
     return state$.take(1).flatMap(state => {
         switch (R.view(lCommandTypeArg, state)) {
             case 'app':
-                return Kefir.fromCallback(cb => cb(new Dashboard({
-                    color: 'green',
-                    minimal: false,
-                    title: 'brookjs :: webpack-dashboard'
-                })))
-                    .flatMap(dashboard =>
-                        actions$.thru(ofType(WEBPACK_DASHBOARD_EVENTS)).flatMap(action =>
-                            Kefir.stream(emitter => {
-                                dashboard.setData(action.payload.events);
-
-                                emitter.end();
-                            })
-                        )
-                    );
+                return actions$.thru(ofType(WEBPACK_COMPILED)).flatMap(({ payload }) =>
+                    ui.log('info', payload.stats.toString({ colors: true }))
+                );
             case 'tdd':
             default:
                 return Kefir.never();
