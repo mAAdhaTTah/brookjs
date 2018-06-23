07dac969d725cb766c5167fdffa857d249bffe75
mAAdhaTTah <jamesorodig@gmail.com>
2018-06-23 11:43:39 -0400

    Export fromReduxStore from brookjs

--- packages/brookjs/src/index.js
+++ packages/brookjs/src/index.js
@@ -4,8 +4,9 @@ import { component, children, events, render,
     containerAttribute, blackboxAttribute, keyAttribute,
     eventAttribute, mapActionTo, renderFromHTML, raf$, RAF } from './component';
 import domDelta from './domDelta';
+import fromReduxStore from './fromReduxStore';
 import { observeDelta, ofType } from './observeDelta';
 
 export { Kefir, children, combineActionReducers, component, events, domDelta,
-    observeDelta, containerAttribute, eventAttribute, ofType, RAF,
+    observeDelta, containerAttribute, eventAttribute, ofType, RAF, fromReduxStore,
     blackboxAttribute, keyAttribute, mapActionTo, render, raf$, renderFromHTML };
