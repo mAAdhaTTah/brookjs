7bcfc7de423d813cd474aa3ea57d4aa681b2e7be
mAAdhaTTah <jamesorodig@gmail.com>
2018-06-26 10:00:37 -0400

    Fix export
    See previous commit.

--- packages/brookjs-silt/src/index.js
+++ packages/brookjs-silt/src/index.js
@@ -4,7 +4,7 @@ import h from './h';
 import loop from './loop';
 import observableOf from './observableOf';
 import view from './view';
-import withRef$ from './withRef$';
+import withRef$ from './withRef';
 
 export { Aggregator, Collector, h,
     h as createElement, loop, observableOf,
