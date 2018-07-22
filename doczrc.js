91b83d6111a11b5ceb145b3da0becb3cf441fba6
mAAdhaTTah <jamesorodig@gmail.com>
2018-07-21 20:15:14 -0400

    Add deployment script for documentation
    Allows us to push the latest site to GitHub pages.

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,7 @@ import pkg from './packages/brookjs/package.json';
 
 export default {
     title: pkg.name,
+    base: '/brookjs/',
     description: pkg.description,
     src: './docs',
     order: 'ascending'
