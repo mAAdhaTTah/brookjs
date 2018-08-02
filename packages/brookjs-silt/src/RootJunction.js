6276d9bd040f9f25417e995bac2fb6c6657872ab
mAAdhaTTah <jamesorodig@gmail.com>
2018-08-02 11:37:13 -0400

    Add RootJunction component
    This is a copy of the Aggregator, which we're going to deprecate. We also
    changed some of the property names. We're going to harmonize on
    `root$`.

---
+++ packages/brookjs-silt/src/RootJunction.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Kefir from 'kefir';
+import PropTypes from 'prop-types';
+import { Provider } from './context';
+import h from './h';
+
+export default class RootJunction extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.root$ = Kefir.pool();
+    }
+
+    componentDidMount() {
+        this.sub = this.props.root$(this.root$);
+    }
+
+    componentWillUnmount() {
+        this.sub && this.sub.unsubscribe();
+    }
+
+    render() {
+        return (
+            <Provider value={this.root$}>
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+RootJunction.propTypes = {
+    children: PropTypes.element.isRequired,
+    root$: PropTypes.func.isRequired
+};
---
+++ packages/brookjs-silt/src/__tests__/RootJunction.spec.js
@@ -0,0 +1,56 @@
+/* eslint-env mocha */
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import { expect, use } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import Kefir from 'kefir';
+import { chaiPlugin } from 'brookjs-desalinate';
+import h from '../h';
+import RootJunction from '../RootJunction';
+
+const { plugin } = chaiPlugin({ Kefir });
+
+configure({ adapter: new Adapter() });
+use(plugin);
+use(sinonChai);
+
+describe('RootJunction', () => {
+    it('should provide aggregated$ as context', () => {
+        const spy = sinon.spy(() => {});
+        mount(
+            <RootJunction root$={spy}>
+                <p>Hello world!</p>
+            </RootJunction>
+        );
+
+        expect(spy).to.have.callCount(1);
+        expect(spy.getCall(0).args[0]).to.be.observable();
+    });
+
+    it('should call unsubscribe when unmounted', () => {
+        const spy = sinon.spy(x => x.observe());
+        const wrapper = mount(
+            <RootJunction root$={spy}>
+                <p>Hello world!</p>
+            </RootJunction>
+        );
+
+        wrapper.unmount();
+
+        expect(spy.getCall(0).args[0]).to.not.be.active();
+    });
+
+    it('should function correctly when nothing returned', () => {
+        const spy = sinon.spy(() => {});
+        const wrapper = mount(
+            <RootJunction root$={spy}>
+                <p>Hello world!</p>
+            </RootJunction>
+        );
+
+        wrapper.unmount();
+
+        expect(spy.getCall(0).args[0]).to.not.be.active();
+    });
+});
