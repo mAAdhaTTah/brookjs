9e5a03e9bc015b0a6785ab60e18ff942d5f8dfc8
mAAdhaTTah <jamesorodig@gmail.com>
2018-10-28 19:49:03 -0400

    Replace "browser" -> "rollup:dist"
    This allows us to configure the output file in each package while
    sharing a single configuration.
    
    Fixes #262.

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ const cjs = {
 export default [
     {
         input: 'src/index.js',
-        output: { name: pkg.name, file: pkg.browser, format: 'umd' },
+        output: { name: pkg.name, file: pkg['rollup:dist'], format: 'umd' },
         plugins: [
             babel(),
             resolve(),
