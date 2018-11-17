4950de7af93a93223e337422f3ac888d865d7173
mAAdhaTTah <jamesorodig@gmail.com>
2018-11-17 10:50:47 -0500

    Add combineReducers and clean up tests
    100% coverage of the eddy module! Add returning `loop.NONE` from one of the
    arrays, to ensure we get coverage of the `!== NONE` branch. Also fixed a bug
    in the `runCommands`, which was calling the original `store.dispatch` instead
    of the wrapped `dispatch` function. This would terminate the continuous dispatch
    of the returned actions.

--- packages/brookjs/src/index.js
+++ packages/brookjs/src/index.js
@@ -1,7 +1,8 @@
-import { eddy, loop } from './eddy';
+import { eddy, loop, combineReducers } from './eddy';
 import { raf$, RAF } from './rAF';
 import fromReduxStore from './fromReduxStore';
 import ofType from './ofType';
 import observeDelta from './observeDelta';
 
-export { observeDelta, ofType, RAF, fromReduxStore, raf$, eddy, loop };
+export { observeDelta, ofType, RAF, fromReduxStore, raf$, eddy, loop,
+    combineReducers };
