d0c33a62995492a7783aa872e935d6cb4fcc1ef8
mAAdhaTTah <jamesorodig@gmail.com>
2018-12-25 18:00:47 -0500

    Add support for testing deltas
    Fixes #114.

--- packages/brookjs-desalinate/src/__tests__/chaiPlugin.spec.js
+++ packages/brookjs-desalinate/src/__tests__/chaiPlugin.spec.js
@@ -1,14 +1,62 @@
 /* eslint-env mocha */
 import Kefir from 'kefir';
-import { expect } from 'chai';
+import { expect, use } from 'chai';
 import chaiPlugin from '../chaiPlugin';
 
-describe('chaiPlugin', () => {
-    it('should be a function', () => {
-        expect(chaiPlugin).to.be.a('function');
-    });
+const { plugin, value } = chaiPlugin({ Kefir });
+use(plugin);
 
-    it('should return an object', () => {
-        expect(chaiPlugin({ Kefir })).to.be.a('object');
+describe('chaiPlugin', () => {
+    describe('delta', () => {
+        const action = { type: 'DO' };
+        const state = { active: true };
+        const delta = ({ respond }) => (action$, state$) =>
+            Kefir.zip([action$, state$]).flatMap(respond);
+
+        it('should send state & action to delta', () => {
+            const services = {
+                respond: ([action, state]) => Kefir.constant({ action, state })
+            };
+
+            expect(delta(services)).to.emitFromDelta([
+                [0, value({ action, state })]
+            ], send => {
+                send(action, state);
+            });
+        });
+
+        it('should provide tick function', () => {
+            const services = {
+                respond: ([action, state]) => Kefir.later(100, { action, state })
+            };
+
+            expect(delta(services)).to.emitFromDelta([
+                [100, value({ action, state })],
+                [200, value({ action, state })],
+            ], (send, tick) => {
+                // One tick
+                send(action, state);
+                tick(100);
+
+                // Broken up ticks
+                send(action, state);
+                tick(50);
+                tick(50);
+            }, {});
+        });
+
+        it('should drain queue', () => {
+            const services = {
+                respond: ([action, state]) => Kefir.later(100, { action, state })
+            };
+
+            expect(delta(services)).to.emitFromDelta([
+                [100, value({ action, state })],
+            ], send => {
+                send(action, state);
+            }, {
+                timeLimit: 100
+            });
+        });
     });
 });
