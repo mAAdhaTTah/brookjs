8435cd680f81acaf13c86c1d610e6b1ab7e7d86f
mAAdhaTTah <jamesorodig@gmail.com>
2019-12-18 22:48:22 -0500

    Verify commands exists before loading them
    Avoids the error message rendering on startup if the user has not
    created any commands.

--- packages/brookjs-cli/bin/beaver.js
+++ packages/brookjs-cli/bin/beaver.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 const path = require('path');
+const { promises: fs } = require('fs');
 const { create } = require('brookjs-cli');
 
 async function main() {
-  const run = await create()
-    .loadCommandsFrom(path.join(process.cwd(), 'commands'))
-    .run(process.argv.slice(2));
+  const app = create();
+
+  const cmdPath = path.join(process.cwd(), 'commands');
+
+  try {
+    if (await fs.stat(cmdPath)) {
+      app = app.loadCommandsFrom(cmdPath);
+    }
+  } catch {}
+
+  const run = await app.run(process.argv.slice(2));
 
   let code;
 
