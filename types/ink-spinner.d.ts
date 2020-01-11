df2d2482c3e0ca0fb9b40fb86df790bce7b50c0e
mAAdhaTTah <jamesorodig@gmail.com>
2020-01-11 13:21:04 -0500

    Update type definitions
    `ink-testing-library` has types on DT, but they're old. I've
    opened a PR to update the main library with these types.
    `ink-spinner` has an open PR to add types, so we'll remove
    both of them as those get merged.

--- types/ink-spinner.d.ts
+++ types/ink-spinner.d.ts
@@ -1,9 +1,53 @@
 declare module 'ink-spinner' {
-  import { SpinnerName } from 'cli-spinners';
+  // Type definitions ported from DefinitelyTyped
+  // Ported by: M4rk9696 <https://github.com/M4rk9696>
+  // Original Definitions by: Łukasz Ostrowski <https://github.com/lukostry>
+  // @TODO(mAAdhaTTah) delete after this is merged: https://github.com/vadimdemedes/ink-spinner/pull/7
+
+  import { Chalk } from 'chalk';
+  import * as cliSpinners from 'cli-spinners';
+  import { Component } from 'react';
+
+  type StringifyPartial<T> = {
+    [P in keyof T]?: string;
+  };
+
+  type BooleansPartial<T> = {
+    [P in keyof T]?: boolean;
+  };
+
+  type TupleOfNumbersPartial<T> = {
+    [P in keyof T]?: [number, number, number];
+  };
+  // Omit taken from https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html
+  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+
+  type ChalkColorModels = Pick<
+    Chalk,
+    'rgb' | 'hsl' | 'hsv' | 'hwb' | 'bgRgb' | 'bgHsl' | 'bgHsv' | 'bgHwb'
+  >;
+  type ChalkKeywordsAndHexes = Pick<
+    Chalk,
+    'keyword' | 'hex' | 'bgKeyword' | 'bgHex'
+  >;
+  type ChalkCommons = Omit<
+    Chalk,
+    | keyof ChalkColorModels
+    | keyof ChalkKeywordsAndHexes
+    | 'constructor'
+    | 'level'
+    | 'enabled'
+  >;
 
   interface SpinnerProps {
-    type: SpinnerName;
+    type?: cliSpinners.SpinnerName;
   }
 
-  export default class Spinner extends React.Component<SpinnerProps> {}
+  type ChalkProps = BooleansPartial<ChalkCommons> &
+    StringifyPartial<ChalkKeywordsAndHexes> &
+    TupleOfNumbersPartial<ChalkColorModels>;
+
+  class Spinner extends Component<SpinnerProps & ChalkProps> {}
+
+  export = Spinner;
 }
--- types/ink-testing-library.d.ts
+++ types/ink-testing-library.d.ts
@@ -1,18 +1,20 @@
 declare module 'ink-testing-library' {
+  // @TODO(mAAdhaTTah) delete after this is merged: https://github.com/vadimdemedes/ink-testing-library/pull/9
   import * as React from 'react';
 
-  interface MockStdIn {
+  export interface MockStdIn {
     write(str: string): void;
   }
 
-  interface Helpers {
-    rerender(tree: React.ReactNode): void;
-    lastFrame(): string;
+  export interface Helpers<P> {
+    rerender<P>(tree: React.ReactElement<P>): void;
     unmount(): void;
     stdin: MockStdIn;
+    frames: Array<string>;
+    lastFrame(): string;
   }
 
-  export function render(node: React.ReactNode): Helpers;
+  export function render<P>(node: React.ReactElement<P>): Helpers<P>;
 
   export function cleanup(): void;
 }
