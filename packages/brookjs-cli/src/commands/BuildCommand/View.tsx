1baf0e358e88cf03c728242b89bbd1daaba41692
mAAdhaTTah <jamesorodig@gmail.com>
2020-02-09 13:05:03 -0500

    Extract components from BuildCommand View
    These can be reused in the Start command.

--- packages/brookjs-cli/src/commands/BuildCommand/View.tsx
+++ packages/brookjs-cli/src/commands/BuildCommand/View.tsx
@@ -1,41 +1,9 @@
-import webpack from 'webpack';
 import Spinner from 'ink-spinner';
-import { Box, Color, useApp } from 'ink';
-import React, { useEffect } from 'react';
-import { format } from 'date-fns';
+import { Box, Color } from 'ink';
+import React from 'react';
 import { ExitError, useExit } from '../../cli';
+import { Built, BuildErrors } from '../components';
 import { State } from './types';
-
-const Asset: React.FC<{
-  name: string;
-  entry: webpack.Stats.ChunkGroup;
-}> = ({ name }) => {
-  return (
-    <Box flexDirection="row" paddingLeft={2}>
-      <Color magentaBright>
-        {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
-        <Box marginRight={2}>🤖</Box>
-        {name}
-      </Color>
-    </Box>
-  );
-};
-
-const Warning: React.FC<{ message: string }> = ({ message }) => {
-  return (
-    <Box>
-      <Color yellow>{message}</Color>
-    </Box>
-  );
-};
-
-const ErrorMsg: React.FC<{ message: string }> = ({ message }) => {
-  return (
-    <Box>
-      <Color redBright>{message}</Color>
-    </Box>
-  );
-};
 
 const Building: React.FC<{}> = () => (
   <Box>
@@ -45,69 +13,6 @@ const Building: React.FC<{}> = () => (
     <Box paddingLeft={1}>Building application...</Box>
   </Box>
 );
-
-const Built: React.FC<{ results: webpack.Stats; watch: boolean }> = ({
-  results,
-  watch
-}) => {
-  const {
-    assets = [],
-    entrypoints = {},
-    builtAt = Date.now(),
-    warnings,
-    errors
-  } = results.toJson('verbose');
-
-  return (
-    <Box flexDirection="column">
-      <Color cyan>{format(builtAt, "'Finished compiling at 'HH:mm:ss")}</Color>
-      <Color cyanBright>
-        Generated {assets.length} asset{assets.length === 1 ? '' : 's'} from
-        entrypoints:
-      </Color>
-      <Box flexDirection="column" marginBottom={1}>
-        {Object.entries(entrypoints).map(([name, entry]) => (
-          <Asset key={name} name={name} entry={entry} />
-        ))}
-      </Box>
-      {warnings.length > 0 && (
-        <Box flexDirection="column" marginBottom={1}>
-          <Box marginBottom={1}>
-            <Color yellowBright>Compilation warnings:</Color>
-          </Box>
-          {warnings.map((warning, i) => (
-            <Warning key={i} message={warning} />
-          ))}
-        </Box>
-      )}
-      {errors.length > 0 && <BuildErrors errors={errors} watch={watch} />}
-    </Box>
-  );
-};
-
-const BuildErrors: React.FC<{ errors: string[]; watch: boolean }> = ({
-  errors,
-  watch
-}) => {
-  const { exit } = useApp();
-
-  useEffect(() => {
-    if (!watch) {
-      exit(new ExitError(1));
-    }
-  }, [watch, exit]);
-
-  return (
-    <Box flexDirection="column" marginBottom={1}>
-      <Box marginBottom={1}>
-        <Color redBright>Compilation errors:</Color>
-      </Box>
-      {errors.map((error, i) => (
-        <ErrorMsg key={i} message={error} />
-      ))}
-    </Box>
-  );
-};
 
 const RCNotLoaded: React.FC = () => {
   useExit(new ExitError(1));
---
+++ packages/brookjs-cli/src/commands/components/webpack.tsx
@@ -0,0 +1,95 @@
+import webpack from 'webpack';
+import { Box, Color, useApp } from 'ink';
+import React, { useEffect } from 'react';
+import { format } from 'date-fns';
+import { ExitError } from '../../cli';
+
+export const Built: React.FC<{
+  results: webpack.Stats;
+  watch: boolean;
+}> = ({ results, watch }) => {
+  const {
+    assets = [],
+    entrypoints = {},
+    builtAt = Date.now(),
+    warnings,
+    errors
+  } = results.toJson('verbose');
+  return (
+    <Box flexDirection="column">
+      <Color cyan>{format(builtAt, "'Finished compiling at 'HH:mm:ss")}</Color>
+      <Color cyanBright>
+        Generated {assets.length} asset{assets.length === 1 ? '' : 's'} from
+        entrypoints:
+      </Color>
+      <Box flexDirection="column" marginBottom={1}>
+        {Object.entries(entrypoints).map(([name, entry]) => (
+          <Asset key={name} name={name} entry={entry} />
+        ))}
+      </Box>
+      {warnings.length > 0 && (
+        <Box flexDirection="column" marginBottom={1}>
+          <Box marginBottom={1}>
+            <Color yellowBright>Compilation warnings:</Color>
+          </Box>
+          {warnings.map((warning, i) => (
+            <Message key={i} level="warning" message={warning} />
+          ))}
+        </Box>
+      )}
+      {errors.length > 0 && <BuildErrors errors={errors} watch={watch} />}
+    </Box>
+  );
+};
+
+export const BuildErrors: React.FC<{ errors: string[]; watch: boolean }> = ({
+  errors,
+  watch
+}) => {
+  const { exit } = useApp();
+
+  useEffect(() => {
+    if (!watch) {
+      exit(new ExitError(1));
+    }
+  }, [watch, exit]);
+
+  return (
+    <Box flexDirection="column" marginBottom={1}>
+      <Box marginBottom={1}>
+        <Color redBright>Compilation errors:</Color>
+      </Box>
+      {errors.map((error, i) => (
+        <Message key={i} level="error" message={error} />
+      ))}
+    </Box>
+  );
+};
+
+export const Asset: React.FC<{
+  name: string;
+  entry: webpack.Stats.ChunkGroup;
+}> = ({ name }) => {
+  return (
+    <Box flexDirection="row" paddingLeft={2}>
+      <Color magentaBright>
+        {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
+        <Box marginRight={2}>🤖</Box>
+        {name}
+      </Color>
+    </Box>
+  );
+};
+
+export const Message: React.FC<{
+  level: 'error' | 'warning';
+  message: string;
+}> = ({ level, message }) => {
+  return (
+    <Box>
+      <Color redBright={level === 'error'} yellow={level === 'warning'}>
+        {message}
+      </Color>
+    </Box>
+  );
+};
